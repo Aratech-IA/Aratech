9a276bd46bd8145c69d0e847a26c56ccc225d8bf
jjehl <julien.jehl@aratech.fr>
2023-04-29 16:19:06 +0200

    update conf

--- asgi.sh
+++ asgi.sh
@@ -1,5 +1,5 @@
 # uvicorn is good for light application with --reload
-uvicorn --ws auto  --host 0.0.0.0 --port 9090 --reload Aratech.asgi:application
+uvicorn --ws auto  --host 0.0.0.0 --port 9090 --reload Aratech_project.asgi:application
 
 # gunicorn is suitable for production with process management
 #gunicorn  -b 0.0.0.0:9090 -w 4 -k uvicorn.workers.UvicornWorker projet.asgi:application
