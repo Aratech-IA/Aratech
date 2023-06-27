ef794b2f091f7f18cac81288af84a07c6cca949f
xrobellet <xavier.robellet@le-campus-numerique.fr>
2023-06-27 16:29:17 +0200

    update settings for contact by email

--- Aratech_project/settings.py
+++ Aratech_project/settings.py
@@ -145,3 +145,13 @@ STATIC_ROOT = '../static_root'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# email contact
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = os.getenv('EMAIL_HOST')
+EMAIL_PORT = os.getenv('EMAIL_PORT')
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+
+
