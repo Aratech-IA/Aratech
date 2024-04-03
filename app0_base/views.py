0a48f10ef98d9fb878f28a3c28d24ed9b5eb5ff8
Nicolas LAMBERT <nicolas.lambert@aratech.fr>
2024-04-03 11:58:24 +0200

    Mise en place CAPTCHA Formilaire contact

--- app0_base/views.py
+++ app0_base/views.py
@@ -1,6 +1,11 @@
+"""views for app0_base"""
+
 from django.core.mail import send_mail, BadHeaderError
 from django.shortcuts import render
 from django.utils.translation import gettext_lazy as _
+from django.conf import settings
+import requests
+
 from Aratech_project.settings import EMAIL_CONTACT
 
 from .forms import ContactForm
@@ -10,31 +15,37 @@ from .forms import ContactForm
 
 # Page Index
 def index(request):
+    """index view"""
     return render(request, 'app0_base/index.html', {"index": True})
 
 
 # Page Team
 def team(request):
+    """Team view"""
     return render(request, 'app0_base/team.html')
 
 
 # Page Expertise
 def expertise(request):
+    """Expertise view"""
     return render(request, 'app0_base/expertise.html')
 
 
 # Page Artificial intellignence
 def ai(request):
+    """AI view"""
     return render(request, 'app0_base/ai.html')
 
 
 # Page Sérénicia
 def serenicia(request):
+    """Sérenicia view"""
     return render(request, 'app0_base/serenicia.html')
 
 
 # Page contact
 def contact(request):
+    """Contact view"""
     error_message = _("Unable to send mail")
     if request.method == 'POST':
         form = ContactForm(request.POST)
@@ -46,29 +57,51 @@ def contact(request):
             from_email = form.cleaned_data["from_email"]
             message = form.cleaned_data['message']
 
-            try:
-                # Envoyer l'e-mail
-                send_mail(
-                    subject,
-                    f'Nous avons reçu un message venant du formulaire contact de Aratech.fr : \n \n  Prenom : {firstname} \n  Nom : {name} \n  Telephone : {phone} \n  Email : {from_email} \n  Sujet : {subject} \n  Message : {message}',
-                    "nicolas.lambert@aratech.fr",
-                    [EMAIL_CONTACT],
-                )
-                # Retourner un message de succès
-                success_message = _("Your message has been sent successfully !")
-                form = ContactForm()
-                return render(request, 'app0_base/contact.html', {'form': form, 'success_message': success_message})
-
-            except BadHeaderError:
-                # En cas d'erreur de l'en-tête du message
-                error_message = _("An error occurred while sending the message")
+            # reCAPTCHA verification
+            recaptcha_token = request.POST.get('g-recaptcha-response')
+            if not recaptcha_token:
+                error_message = _('Invalid captcha')
                 return render(request, 'app0_base/contact.html', {'form': form, 'error_message': error_message})
 
-            except OSError:
-                # En cas d'erreur lors de l'envoie du message
-                return render(request, 'app0_base/contact.html', {'form': form, 'error_message': error_message})
+            url = 'https://www.google.com/recaptcha/api/siteverify'
+            data = {
+                'secret': settings.CAPTCHA_SECRETKEY,
+                'response': recaptcha_token,
+            }
+            response = requests.post(url, data=data, timeout=5)
 
-        return render(request, 'app0_base/contact.html', {'form': form, 'error_message': error_message})
+            if response.status_code == 200:
+                json_response = response.json()
+                if json_response['success']:
+                    # Score de risque acceptable, traiter le formulaire
+                    try:
+                        # Envoyer l'e-mail
+                        send_mail(
+                            subject,
+                            f'Nous avons reçu un message venant du formulaire contact de Aratech.fr : \n \n  Prenom : {firstname} \n  Nom : {name} \n  Telephone : {phone} \n  Email : {from_email} \n  Sujet : {subject} \n  Message : {message}',
+                            "nicolas.lambert@aratech.fr",
+                            [EMAIL_CONTACT],
+                        )
+                        # Retourner un message de succès
+                        success_message = _("Your message has been sent successfully !")
+                        form = ContactForm()
+                        return render(request, 'app0_base/contact.html', {'form': form, 'success_message': success_message})
+                    except BadHeaderError:
+                        error_message = _("An error occurred while sending the message")
+                    except OSError:
+                        pass  # Handle other potential errors differently
+
+                else:
+                    # Score de risque suspect, refuser le formulaire
+                    error_message = _('The reCAPTCHA indicates suspicious activity. Try Again...')
+            else:
+                # Erreur lors de la requête à l'API reCAPTCHA
+                error_message = _('An error occurred while verifying the reCAPTCHA...')
+
+            return render(request, 'app0_base/contact.html', {'form': form, 'error_message': error_message})
+
+        else:
+            form = ContactForm()
 
     else:
         form = ContactForm()
@@ -78,4 +111,5 @@ def contact(request):
 
 # Page Legal Notice
 def legalnotice(request):
+    """Legal Notice view"""
     return render(request, 'app0_base/legalnotice.html')
