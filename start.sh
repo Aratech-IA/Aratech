d6a1bb4d3b0a2a6c39bebe6af720c11b5b024187
Julien JEHL <julien.jehl@roboticia.com>
2025-01-25 11:04:16 +0100

    start remote

--- start.sh
+++ start.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
-python3 /App/Aratech/manage.py collectstatic --noinput > /App/log_collect_static-$(date +"%Y_%m_%d_%T").log
+python /App/manage.py collectstatic --noinput > /App/log_collect_static-$(date +"%Y_%m_%d_%T").log
+python /App/manage.py migrate > /App/log_migrate-$(date +"%Y_%m_%d_%T").log
 /App/asgi.sh
